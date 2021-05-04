c088696c39244c2274b768a3771048de50d01dd3
Nikola <nikola.mlinaric93@gmail.com>
2021-05-04 16:43:41 +0200

    Add payable fallback to test ERC721

--- contracts/Test_NFT.sol
+++ contracts/Test_NFT.sol
@@ -5,10 +5,12 @@ pragma solidity ^0.8.1;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract TestNFT is ERC721("TEST", "TST") {
+contract TestNFT is ERC721("TEST", "TEST") {
 
     function mint(address account, uint256 tokenId) public {
         _mint(account, tokenId);
     }
+
+    receive() external payable {}
 
 }
