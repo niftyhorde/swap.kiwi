ad3eb349f6c31effcfa854d84b6450afdf6a27c7
Belma Gutlic <belma.dzi@gmail.com>
2021-06-18 16:00:43 +0200

    Revert "Revert CryptoPunks support "

--- contracts/ICryptoPunks.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.8.1;
-
-interface ICryptoPunks {
-  function punkIndexToAddress(uint punkIndex) external view returns (address);
-
-  function transferPunk(address punkIndex, uint transferPunkIndex) external;
-}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,4 @@
-import { HardhatUserConfig } from "hardhat/types";
+import {HardhatUserConfig} from "hardhat/types";
 import accounts from "./test/Accounts"
 import dotenv from 'dotenv';
 
@@ -30,11 +30,7 @@ const config: HardhatUserConfig = {
     hardhat: {
       loggingEnabled: false,
       live: false,
-      accounts: accounts,
-      forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_APP_ID}`,
-        blockNumber: 12503592
-      }
+      accounts: accounts
     },
     coverage: {
       url: 'http://127.0.0.1:5458'
