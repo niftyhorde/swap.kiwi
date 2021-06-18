6a23ac6b93778a8551f6f6213898a0b2685c611c
Nikola <nikola.mlinaric93@gmail.com>
2021-06-18 10:27:07 +0200

    Remove license and unused imports

--- contracts/SwapKiwi.sol
+++ contracts/SwapKiwi.sol
@@ -1,10 +1,7 @@
-// SPDX-License-Identifier: UNLICENSED
-
 pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
