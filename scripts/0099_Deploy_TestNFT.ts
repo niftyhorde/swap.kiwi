08c5e7c6199f9bcb1de81e99b875f86ba29be79d
Nikola <nikola.mlinaric93@gmail.com>
2021-06-18 09:44:27 +0200

    Skip deploying TestNFT to mainnet

--- scripts/0099_Deploy_TestNFT.ts
+++ scripts/0099_Deploy_TestNFT.ts
@@ -1,11 +1,17 @@
-import {HardhatRuntimeEnvironment} from "hardhat/types";
-import {DeployFunction} from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
 
 const deployFunc: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const {deployments, getNamedAccounts} = hre;
-  const {deploy} = deployments;
-  const {deployer} = await getNamedAccounts();
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
 
-  await deploy("TestNFT", {from: deployer});
+  await deploy("TestNFT", { from: deployer });
 }
 export default deployFunc;
+
+
+// skip deployment if deploying to mainnet
+deployFunc.skip = async (hre: HardhatRuntimeEnvironment) => {
+  return hre.network.name === 'mainnet'
+};
