b96a11b2a1db3f2743bf2d61e4ea05aa345dc63f
Nikola <nikola.mlinaric93@gmail.com>
2021-06-20 17:03:58 +0200

    Fix contract deplyoment config

--- scripts/00_Deploy_SwapKiwi.ts
+++ scripts/00_Deploy_SwapKiwi.ts
@@ -8,29 +8,32 @@ const deployFunc: DeployFunction = async function (hre: HardhatRuntimeEnvironmen
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  let mainnetDeploymentGasPrice: BigNumber | string | undefined;
   let contractOwnerAddress: string | undefined;
 
   const initialAppFee = ethers.utils.parseEther(process.env.SWAP_KIWI_FEE ?? "0.0025");
 
   if (hre.network.name === 'mainnet') {
-    mainnetDeploymentGasPrice = BigNumber.from(process.env.MAINNET_DEPLOYMENT_GAS_PRICE);
+    const mainnetDeploymentGasPrice = BigNumber.from(process.env.MAINNET_DEPLOYMENT_GAS_PRICE);
     contractOwnerAddress = process.env.CONTRACT_OWNER_ADDRESS;
 
     if (!mainnetDeploymentGasPrice || !contractOwnerAddress) {
       throw Error("Missing mainnet deployment owner address and/or gas price env");
     }
+    await deploy("SwapKiwi", {
+      from: deployer, args: [
+        initialAppFee,
+        contractOwnerAddress
+      ],
+      gasPrice: mainnetDeploymentGasPrice
+    });
   } else {
-    mainnetDeploymentGasPrice = String(hre.network.config.gasPrice);
     contractOwnerAddress = deployer;
+    await deploy("SwapKiwi", {
+      from: deployer, args: [
+        initialAppFee,
+        contractOwnerAddress
+      ],
+    });
   }
-
-  await deploy("SwapKiwi", {
-    from: deployer, args: [
-      initialAppFee,
-      contractOwnerAddress
-    ],
-    gasPrice: mainnetDeploymentGasPrice
-  });
 }
 export default deployFunc;
