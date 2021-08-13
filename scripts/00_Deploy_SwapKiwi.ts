6173a2fe241a37b2d6dc15ee59152e15f0c1fcfe
morrigan <belma.dzi@gmail.com>
2021-08-13 17:34:54 +0200

    Remove mainnet deployment hardcoded gasprice

--- scripts/00_Deploy_SwapKiwi.ts
+++ scripts/00_Deploy_SwapKiwi.ts
@@ -1,7 +1,6 @@
 import { ethers } from 'hardhat';
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { BigNumber } from 'ethers';
 
 const deployFunc: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
@@ -13,18 +12,16 @@ const deployFunc: DeployFunction = async function (hre: HardhatRuntimeEnvironmen
   const initialAppFee = ethers.utils.parseEther(process.env.SWAP_KIWI_FEE ?? "0.0025");
 
   if (hre.network.name === 'mainnet') {
-    const mainnetDeploymentGasPrice = BigNumber.from(process.env.MAINNET_DEPLOYMENT_GAS_PRICE);
     contractOwnerAddress = process.env.CONTRACT_OWNER_ADDRESS;
 
-    if (!mainnetDeploymentGasPrice || !contractOwnerAddress) {
-      throw Error("Missing mainnet deployment owner address and/or gas price env");
+    if (!contractOwnerAddress) {
+      throw Error("Missing mainnet deployment owner address");
     }
     await deploy("SwapKiwi", {
       from: deployer, args: [
         initialAppFee,
         contractOwnerAddress
       ],
-      gasPrice: mainnetDeploymentGasPrice
     });
   } else {
     contractOwnerAddress = deployer;
