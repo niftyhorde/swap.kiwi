85abc6d85abb993c9b007a4cc01729163f22b8af
andersonlee725 <anderson@chainsafe.io>
2021-12-01 06:57:12 -0800

    fix comment

--- scripts/00_Deploy_SwapKiwiV1.ts
+++ scripts/00_Deploy_SwapKiwiV1.ts
@@ -11,7 +11,7 @@ const deployFunc: DeployFunction = async function (hre: HardhatRuntimeEnvironmen
 
   const initialAppFee = ethers.utils.parseEther(process.env.SWAP_KIWI_FEE ?? "0.0025");
 
-  if (hre.network.name === 'mainnet') {
+  if (hre.network.name === 'mainnet' && process.env.DEPLOY_V1 === 'true') {
     contractOwnerAddress = process.env.CONTRACT_OWNER_ADDRESS;
 
     if (!contractOwnerAddress) {
@@ -23,7 +23,7 @@ const deployFunc: DeployFunction = async function (hre: HardhatRuntimeEnvironmen
         contractOwnerAddress
       ],
     });
-  } else {
+  } else if (hre.network.name != 'mainnet') {
     contractOwnerAddress = deployer;
     await deploy("SwapKiwiV1", {
       from: deployer, args: [
